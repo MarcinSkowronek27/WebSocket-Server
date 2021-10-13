040656524bc6acb1a4e6c48ae8a4ba525be53aab
Marcin Skowronek <marcinfoto35@op.pl>
2021-10-13 12:12:43 +0200

    Show app in website

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '/client/')));
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/public/index.html'));
+  res.sendFile(path.join(__dirname, '/client/index.html'));
 });
 
-app.use(express.static(path.join(__dirname, '/client/public')));
 
 const messages = [];
 
