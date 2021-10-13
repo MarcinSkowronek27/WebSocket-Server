59aa47fb808b866d6d058960d8387701eb36a1c6
Marcin Skowronek <marcinfoto35@op.pl>
2021-10-13 10:14:31 +0200

    Done stage 4

--- client/app.js
+++ client/app.js
@@ -1,6 +1,26 @@
 {
   'use strict';
 
-  const test = document.querySelector('#welcome-form');
-  console.log(test);
+  const loginForm = document.querySelector('#welcome-form');
+  const messagesSection = document.querySelector('#messages-section');
+  const messagesList = document.querySelector('#messages-list');
+  const addMessageForm = document.querySelector('#add-messages-form');
+  const userNameInput = document.querySelector('#username');
+  const messageContentInput = document.querySelector('#message-content');
+  // console.log(messageContentInput);
+
+  let userName;
+
+  function login(event) {
+    event.preventDefault();
+    if (userNameInput === '') {
+      alert('Uzupełnij pole nazwy');
+    } else {
+      userName = userNameInput;
+      loginForm.classList.remove('show');
+      messagesSection.classList.add('show');
+    }
+  }
+
+  loginForm.addEventListener('submit', login());
 }
