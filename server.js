10cffeb98870e639b8ab6c4fb4f918ff82682511
Marcin Skowronek <marcinfoto35@op.pl>
2021-10-13 12:10:42 +0200

    Next combinations

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ const path = require('path');
 const app = express();
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/index.html'));
+  res.sendFile(path.join(__dirname, '/client/public/index.html'));
 });
 
-app.use(express.static(path.join(__dirname, '/client/style.css')));
-app.use(express.static(path.join(__dirname, '/client/app.js')));
+app.use(express.static(path.join(__dirname, '/client/public')));
 
 const messages = [];
 
