8362ad4677ac299c3f6cd0952a7781a64710dba7
Marcin Skowronek <marcinfoto35@op.pl>
2021-10-14 16:12:32 +0200

    Add sockets

--- client/app.js
+++ client/app.js
@@ -1,60 +1,62 @@
-{
-  'use strict';
+const socket = io();
 
-  const loginForm = document.querySelector('#welcome-form');
-  const messagesSection = document.querySelector('#messages-section');
-  const messagesList = document.querySelector('#messages-list');
-  const addMessageForm = document.querySelector('#add-messages-form');
-  const userNameInput = document.querySelector('#username');
-  const messageContentInput = document.querySelector('#message-content');
-  // console.log(messageContentInput);
+socket.on('message', ({ author, content }) => addMessage(author, content));
 
-  let userName;
+'use strict';
 
-  function login(event) {
-    event.preventDefault();
-    if (userNameInput.value === '') {
-      alert('Uzupełnij pole nazwy');
-    } else {
-      userName = userNameInput;
-      loginForm.classList.remove('show');
-      messagesSection.classList.add('show');
-    }
+const loginForm = document.querySelector('#welcome-form');
+const messagesSection = document.querySelector('#messages-section');
+const messagesList = document.querySelector('#messages-list');
+const addMessageForm = document.querySelector('#add-messages-form');
+const userNameInput = document.querySelector('#username');
+const messageContentInput = document.querySelector('#message-content');
+// console.log(messageContentInput);
+
+let userName;
+
+function login(event) {
+  event.preventDefault();
+  if (userNameInput.value === '') {
+    alert('Uzupełnij pole nazwy');
+  } else {
+    userName = userNameInput;
+    loginForm.classList.remove('show');
+    messagesSection.classList.add('show');
   }
+}
 
-  function addMessage(author, content) {
-    // mój pomysł na ten kod
-    // let list = document.createElement('li');
-    // let heading = document.createElement('h3');
-    // let message = document.createElement('div');
-    // messagesList.appendChild(list);
-    // list.appendChild(heading);
-    // list.appendChild(message);
-    // heading.innerHTML = { author };
-    // message.innerHTML = { textMessage };
+function addMessage(author, content) {
+  // mój pomysł na ten kod
+  // let list = document.createElement('li');
+  // let heading = document.createElement('h3');
+  // let message = document.createElement('div');
+  // messagesList.appendChild(list);
+  // list.appendChild(heading);
+  // list.appendChild(message);
+  // heading.innerHTML = { author };
+  // message.innerHTML = { textMessage };
 
-    const message = document.createElement('li');
-    message.classList.add('message');
-    message.classList.add('message--received');
-    if(author === userName) message.classList.add('message--self');
-    message.innerHTML = `
-      <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if (author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+      <h3 class="message__author">${userName === author ? 'You' : author}</h3>
       <div class="message__content">
         ${content}
       </div>
     `;
-    messagesList.appendChild(message);
+  messagesList.appendChild(message);
 
-  }
-
-  loginForm.addEventListener('submit', login);
-  addMessageForm.addEventListener('submit', function sendMessage(event) {
-    event.preventDefault();
-    if (messageContentInput.value === '') {
-      alert('Uzupełnij pole wiadomości');
-    } else {
-      addMessage(userName, messageContentInput.value);
-      messageContentInput.value = '';
-    }
-  });
 }
+
+loginForm.addEventListener('submit', login);
+addMessageForm.addEventListener('submit', function sendMessage(event) {
+  event.preventDefault();
+  if (messageContentInput.value === '') {
+    alert('Uzupełnij pole wiadomości');
+  } else {
+    addMessage(userName, messageContentInput.value);
+    messageContentInput.value = '';
+  }
+});
